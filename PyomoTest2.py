e832b1e27fb0539123768dac82170c0d8baabbe0
Jose Daniel Lara <jdlara@berkeley.edu>
2016-02-21 20:30:44 -0800

    Flow_models

--- PyomoTest2.py
+++ PyomoTest2.py
@@ -3,10 +3,10 @@
 
 # Import
 from pyomo.environ import *
- 
+
 # Creation of a Concrete Model
 model = ConcreteModel()
- 
+
 ## Define sets ##
 #  Sets
 #       i   canning plants   / seattle, san-diego /
@@ -14,7 +14,10 @@ model = ConcreteModel()
 cit1=['seattle','san-diego']
 model.i = Set(initialize=cit1, doc='Canning plans')
 model.j = Set(initialize=['new-york','chicago', 'topeka'], doc='Markets')
- 
+model.r = Set(model.i*model.j)
+
+model.r.pprint()
+
 ## Define parameters ##
 #   Parameters
 #       a(i)  capacity of plant i in cases
@@ -31,12 +34,12 @@ model.b = Param(model.j, initialize={'new-york':325,'chicago':300,'topeka':275},
 #      seattle          2.5           1.7          1.8
 #      san-diego        2.5           1.8          1.4  ;
 dtab = {
-    ('seattle',  'new-york') : 2.5,
-    ('seattle',  'chicago')  : 1.7,
-    ('seattle',  'topeka')   : 1.8,
-    ('san-diego','new-york') : 2.5,
-    ('san-diego','chicago')  : 1.8,
-    ('san-diego','topeka')   : 1.4,
+    ('seattle',  'new-york'): 2.5,
+    ('seattle',  'chicago'): 1.7,
+    ('seattle',  'topeka'): 1.8,
+    ('san-diego', 'new-york'): 2.5,
+    ('san-diego', 'chicago'): 1.8,
+    ('san-diego', 'topeka'): 1.4,
     }
 model.d = Param(model.i, model.j, initialize=dtab, doc='Distance in thousands of miles')
 #  Scalar f  freight in dollars per case per thousand miles  /90/ ;
@@ -46,26 +49,26 @@ model.f = Param(initialize=90, doc='Freight in dollars per case per thousand mil
 def c_init(model, i, j):
   return model.f * model.d[i,j] / 1000
 model.c = Param(model.i, model.j, initialize=c_init, doc='Transport cost in thousands of dollar per case')
- 
+
 ## Define variables ##
 #  Variables
 #       x(i,j)  shipment quantities in cases
 #       z       total transportation costs in thousands of dollars ;
 #  Positive Variable x ;
 model.x = Var(model.i, model.j, bounds=(0.0,None), doc='Shipment quantities in case')
- 
+
 ## Define contrains ##
 # supply(i)   observe supply limit at plant i
 # supply(i) .. sum (j, x(i,j)) =l= a(i)
 def supply_rule(model, i):
   return sum(model.x[i,j] for j in model.j) <= model.a[i]
 model.supply = Constraint(model.i, rule=supply_rule, doc='Observe supply limit at plant i')
-# demand(j)   satisfy demand at market j ;  
+# demand(j)   satisfy demand at market j ;
 # demand(j) .. sum(i, x(i,j)) =g= b(j);
 def demand_rule(model, j):
-  return sum(model.x[i,j] for i in model.i) >= model.b[j]  
+  return sum(model.x[i,j] for i in model.i) >= model.b[j]
 model.demand = Constraint(model.j, rule=demand_rule, doc='Satisfy demand at market j')
- 
+
 ## Define Objective and solve ##
 #  cost        define objective function
 #  cost ..        z  =e=  sum((i,j), c(i,j)*x(i,j)) ;
@@ -74,13 +77,13 @@ model.demand = Constraint(model.j, rule=demand_rule, doc='Satisfy demand at mark
 def objective_rule(model):
   return sum(model.c[i,j]*model.x[i,j] for i in model.i for j in model.j)
 model.objective = Objective(rule=objective_rule, sense=minimize, doc='Define objective function')
- 
- 
+
+
 ## Display of the output ##
 # Display x.l, x.m ;
 def pyomo_postprocess(options=None, instance=None, results=None):
   model.x.display()
- 
+
 # This is an optional code path that allows the script to be run outside of
 # pyomo command-line.  For example:  python transport.py
 if __name__ == '__main__':
