21616761028017009c40f75c10f0640af86c422e
Jose Daniel Lara <jdlara@berkeley.edu>
2016-05-18 19:40:57 -0700

    Pos-Pre and Exchanges

--- CenceScrapping.py
+++ CenceScrapping.py
@@ -257,27 +257,135 @@ for year_i in range(2015, 2016):
 df_anual_pre = df_anual.fillna(0)
 df_anual_pre.to_csv('df_anual_pre.csv', encoding='utf-8')
 
-# This portion of the code will stract the info from the exchanges
-#driver.quit()
-#driver = init_driver()
+# Code for the exchanges
+driver.quit()
+driver = init_driver()
 
-#driver.get("http://appcenter.grupoice.com/CenceWeb/CenceIntercambios.jsf?init=true")
 
-#html = driver.page_source
+def proc_html_data(transfer):
+    transfer = transfer.replace(",", ".")
+    try:
+        found = re.findall('&lt;set value=\'(.+?)\'/&gt;', transfer)
+    except AttributeError:
+        found = ''
 
-#html[html.find('var chart_graficoIntercambios'):html.find('<!--END Script Block for Chart graficoIntercambios-->')]
+    nfound = []
+    for x in found:
+        nfound.append(float(x.encode('ascii', 'ignore')))
 
-#Header = html[html.find('categories'):html.find('dataset seriesName=\'Real Norte\'')]
+    df_transfer = pd.DataFrame(nfound)
+    return df_transfer
 
-#hfound = re.findall(';&lt;category label=(.+?)showLabel', Header)
+df_anual = pd.DataFrame()
+driver.get("http://appcenter.grupoice.com/CenceWeb/CenceIntercambios.jsf?init=true")
+calendar = driver.wait.until(EC.presence_of_element_located((By.ID, "formIntercambios:pickFechaInputDate")))
+calendar.click()
 
-#RN = html[html.find('seriesName=\'Real Norte\''):html.find(';dataset seriesName=\'Real Sur\'')]
+box = driver.wait.until(EC.presence_of_element_located((By.CLASS_NAME, "rich-calendar-month")))
+box.click()
 
-#RN = RN.replace(",", ".")
+y = 2
+for year_i in range(2014, 2016):
 
-#found = re.findall('&lt;set value=\'(.+?)\'/&gt;', RN)
+    print "Year Loop"
+    print year_i
 
-#nfound = []
-#for x in found:
-#    nfound.append(float(x.encode('ascii', 'ignore')))
-#Header.encode('ascii', 'ignore')
+    year = driver.wait.until(EC.presence_of_element_located((By.ID, "formIntercambios:pickFechaDateEditorLayoutY" + str(y))))
+    year.click()
+
+    time.sleep(1)
+
+    df_month = pd.DataFrame()
+    for month_i in range(5, 7):
+
+        print ' Month loop ' + str(month_i)
+
+        month = driver.wait.until(EC.presence_of_element_located((By.ID, "formIntercambios:pickFechaDateEditorLayoutM" + str(month_i - 1))))
+        month.click()
+
+        time.sleep(1)
+
+        ok = driver.wait.until(EC.presence_of_element_located((By.ID, "formIntercambios:pickFechaDateEditorButtonOk")))
+        ok.click()
+
+        time.sleep(1)
+
+        day_i = driver.wait.until(EC.presence_of_element_located((By.ID, "formIntercambios:pickFechaDayCell15")))
+        day_i.click()
+        calendar.click()
+        calendar.send_keys(Keys.RETURN)
+
+        time.sleep(1)
+
+        print ' day loop'
+
+        if month_i == 1:
+            number_of_days = 31
+        elif month_i == 2:
+            number_of_days = 28
+        elif month_i == 3:
+            number_of_days = 31
+        elif month_i == 4:
+            number_of_days = 30
+        elif month_i == 5:
+            number_of_days = 31
+        elif month_i == 6:
+            number_of_days = 30
+        elif month_i == 7:
+            number_of_days = 30
+        elif month_i == 8:
+            number_of_days = 31
+        elif month_i == 9:
+            number_of_days = 30
+        elif month_i == 10:
+            number_of_days = 31
+        elif month_i == 11:
+            number_of_days = 30
+        elif month_i == 12:
+            number_of_days = 31
+
+        for day_i in range(1, 4):
+
+            print "     " + str(day_i) + " " + str(month_i) + " " + str(year_i)
+
+            calendar = driver.wait.until(EC.presence_of_element_located((By.ID, "formIntercambios:pickFechaInputDate")))
+            calendar.click()
+            day = driver.wait.until(EC.presence_of_element_located((By.ID, "formIntercambios:pickFechaDayCell" + str(day_i + (datetime(year_i, month_i, 1).weekday() - 1)))))
+            time.sleep(1)
+            day.click()
+            calendar.click()
+            calendar.send_keys(Keys.RETURN)
+            time.sleep(1)
+            html = driver.page_source
+            df_day = pd.DataFrame()
+            Real_Norte_html = html[html.find('seriesName=\'Real Norte\''):html.find(';dataset seriesName=\'Real Sur\'')]
+            Real_Sur_html = html[html.find('seriesName=\'Real Sur\''):html.find(';dataset seriesName=\'Programado Norte\'')]
+            Programado_Norte_html = html[html.find('seriesName=\'Programado Norte\''):html.find(';dataset seriesName=\'Programado Sur\'')]
+            Programado_Sur_html = html[html.find('seriesName=\'Programado Sur\''):html.find(';/dataset&gt;&lt;styles&gt;&lt;definition&gt;&lt;stylename=\'EstiloTitulo\'')]
+            df_real_norte = proc_html_data(Real_Norte_html)
+            df_real_sur = proc_html_data(Real_Sur_html)
+            df_programado_norte = proc_html_data(Programado_Norte_html)
+            df_programado_sur = proc_html_data(Programado_Sur_html)
+            df_day = df_day.append(df_real_norte.T)
+            df_day = df_day.append(df_real_sur.T)
+            df_day = df_day.append(df_programado_norte.T)
+            df_day = df_day.append(df_programado_sur.T)
+            df_day = df_day.T
+            datetime_list = []
+            for hour in range(0, 24):
+                for minute in range(0, 46, 15):
+                    datetime_list.append(datetime(year_i, month_i, day_i, int(hour), int(minute)))
+            df_day['datetime'] = datetime_list
+            df_day = df_day.set_index('datetime')
+            df_month = df_month.append(df_day)
+        calendar = driver.wait.until(EC.presence_of_element_located((By.ID, "formIntercambios:pickFechaInputDate")))
+        calendar.click()
+        box = driver.wait.until(EC.presence_of_element_located((By.CLASS_NAME, "rich-calendar-month")))
+        box.click()
+
+    time.sleep(5)
+    df_anual = df_anual.append(df_month)
+    y = 5
+df_intercambios = df_anual.fillna(0)
+df_intercambios.columns = ['Real_Norte', 'Real_Sur', 'Programado_Norte', 'Programado_Sur']
+df_intercambios.to_csv('df_intercambios.csv', encoding='utf-8')
